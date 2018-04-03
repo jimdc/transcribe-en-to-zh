1e60fdc2960bd8f5e004e81e9d02cc8c676f780f
James Carroll <jamescarroll@protonmail.ch>
2018-04-03 17:06:49 -0400

    use module.exports

--- transcribe.js
+++ transcribe.js
@@ -2,17 +2,7 @@ const natural = require('natural');
 const tokenizer = new natural.WordTokenizer();
 const stemmer = natural.PorterStemmer;
 const metaphone = natural.Metaphone;
-
-const _b_ = {initial: 'b', final: null, zi: '布', pinyin: 'bù'};
-const _di = {initial: 'd', final: 'ɪ', zi: '迪', pinyin: 'dí'};
-const __r = {initial: null, final: 'r', zi: '尔', pinyin: 'ěr'};
-const __l = {initial: null, final: 'l', zi: '尔', pinyin: 'ěr'};
-const _is = {initial: 'ɪ', final: 's', zi: '西', pinyin: 'xī'};
-const __t = {initial: null, final: 't', zi: '特', pinyin: 'tè'};
-const _da = {initial: 'd', final: 'ɒ', zi: '多', pinyin: 'duō'};
-
-const dearest_ipa = 'ˈdɪɹɪst';
-const doll_ipa = 'dɒl';
+const chineseSound = require('./ChineseSound');
 
 const myArgs = process.argv.slice(2);
 const arg1 = myArgs[0];
@@ -30,37 +20,10 @@ if (arg1 === undefined) {
 
 console.log(`Raw input was: ${arg1}`);
 
-//dearest doll --> TRST_TL
-function in_zh(token) {
-  let result = [];
-  for(let i = 0; i < token.length; i++) {
-    //Yeah, this is per-token not per-phoneme. Something to fix.
-    const is_initial = (i == 0);
-    const is_final = (i == token.length);
-    switch(token.charAt(i)) {
-      case "T": 
-        if (is_initial) result.push(_di); 
-        else if (is_final) result.push(__t); 
-        else result.push(null);
-        break;
-      case "S":
-        result.push(_is);
-        break;
-      case "R":
-      case "L":
-        result.push(__r);
-        break;
-      default: 
-        result.push(null);
-    }
-  }
-  return result;
-}
-
 metaphone.attach();
 const phonemes = arg1.tokenizeAndPhoneticize();
 phonemes.forEach(function(value, key, phonemes) {
-    const transcribed = in_zh(value);
+    const transcribed = chineseSound.toChineseSounds(value);
     let characters = [];
     let romans = [];
 
@@ -75,8 +38,3 @@ phonemes.forEach(function(value, key, phonemes) {
     console.log(`${value} => ${transcribed_readable}`);
 });
 
-/*
-const zh = in_zh(arg1);
-if (zh !== null) {
-  console.log(`It may be transcribed as ${zh[0]} (${zh[1]})`);
-}*/
