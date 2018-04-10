0515d0c1cadc05a3fc6bb1a249cded1f18db0d24
James Carroll <jamescarroll@protonmail.ch>
2018-04-10 10:56:01 -0400

    add failing colspan test: only one shang4, zhan1 not found

--- test/ChineseSound.test.ts
+++ test/ChineseSound.test.ts
@@ -27,5 +27,22 @@ describe('findChineseSounds test', () => {
     expect(result.initialSound).to.equal('z / dz');
   });
 
+  it('It finds shang4 (unique but colspan=2)', () => {
+    const results = chineseSound.find('尚');
+    expect(results.length).to.equal(2);
+    results.forEach(function(element) {
+      expect(element.initialSound).to.equal('ʃ');
+    });
+  });
+
+  it('It finds the two sounds right after shang4', () => {
+    const resultAen = chineseSound.find('詹');
+    const resultAhn = chineseSound.find('章');
+    expect(resultAen.initialSound).to.equal('dʒ');
+    expect(resultAhn.initialSound).to.equal('dʒ');
+    expect(resultAen.initialSound).to.equal('æn, ʌn, æŋ');
+    expect(resultAhn.initialSound).to.equal('ɑn, aʊn, ʌŋ, ɔn, ɒn, ɒŋ');
+  });
+
 });
 
